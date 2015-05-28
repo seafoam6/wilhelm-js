5282f799f6c1281173bdbb0a60242312df811715
seafoam6 <steve@stevebarman.com>
2015-05-27 20:59:36 -0400

    removes Nicholas Cage

--- js/vanilla.wilhelm.js
+++ js/vanilla.wilhelm.js
@@ -2,32 +2,13 @@
   var audioElement = document.createElement('audio');
   audioElement.setAttribute('src', 'wilhelm.mp3');
   var isPlaying = false;
-  var imgs = document.querySelectorAll("img");
-  var oldSrcs = [];
-
-  for(var i = 0; i < imgs.length; i++) {
-    oldSrcs.push(imgs[i].getAttribute("src"));
-  }
 
   window.onresize = function() {
-    var resizeTo;
-
     if (!isPlaying){
       isPlaying=true;
       audioElement.play();
       isPlaying=false;
     }
-
-    oldSrcs.forEach(function(el, index) {
-      imgs[index].setAttribute("src","http://placecage.com/c/" + imgs[index].offsetWidth + "/" + imgs[index].offsetHeight);
-    });
-
-    if(resizeTo) clearTimeout(resizeTo);
-    resizeTo = setTimeout(function() {
-      oldSrcs.forEach(function(el,index) {
-        imgs[index].setAttribute("src",el);
-      });
-    }, 500);
   };
-
+  
 })();
